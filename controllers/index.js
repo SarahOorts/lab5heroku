0f0eb0dea7426438fce5af4ebb8cc89fe065fda5
SarahOorts <r0657122@student.thomasmore.be>
2021-03-24 13:24:51 +0100

    (fix) - add api & v1 folder by controllers, changed structure GET of message 1 with if statement to include GET 4, changed postUser to check if Heroku works

---
+++ controllers/index.js
@@ -0,0 +1,74 @@
+function getM(req, res){
+    //mongoDB return messages
+   // zonder mongoDB res { message : "GETTING messages"}
+    res.json({
+        status : "succes",
+        message : "GETTING messages",
+    });
+}
+
+function getMId(req, res){
+    // res.send("Hello World!")
+  let id = req.params.id;
+  //mongoDB message met id
+  // zonder mongoDB res { message : `GETTING messages with ID ${id}`}
+  res.json({
+        status : "succes",
+        message : `GETTING messages with ID ${id}`,
+  }); 
+}
+
+function postUser(req, res){
+    // res.send("Hello World!")
+    //mongoDB JSON-object ontv, local storage
+    // body { message: { “user”: “Pikachu”, “text”: “nodejs isn’t hard, or is it?” } }
+    //zonder mongoDB res {“message”: “POSTING a new message for user Pikachu”}
+    //naam dynamisch invullen + teruggeven
+    let user = req.params.user;
+    res.json({
+        status : "succes",
+        message : `POSTING a new message for user ${user}`,
+    });
+}
+
+function putId(req, res){
+    //mongoDB JSON local storage update
+    //zonder mongoDB res  {“message”: “UPDATING a message with id id”}
+    let id = req.params.id;
+  
+    res.json({
+        status : "succes",
+        message : `UPDATING a message with ID ${id}`,
+    }); 
+}
+
+function deleteId(req, res){
+    //mongoDB remove message met id, res {“status”: “success”, “message”: “The message was removed”}
+    //zonder mongoDB res  {“message”: “DELETING a message with id id”}
+    let id = req.params.id;
+  
+    res.json({
+        status : "succes",
+        message : `DELETING a message with ID ${id}`,
+    }); 
+  }
+
+  function getUsername(req, res){ 
+    //mongoDB return messages user
+    //req.params.username!!
+    //zonder mongoDB res  {“message”: “GETTING message for username username”}
+    let username = req.params.username;
+    
+    res.json({
+        status : "succes",
+        message : `GETTING message for username ${username}`,
+    }); 
+    
+  }
+
+module.exports.getM = getM;
+module.exports.getMId = getMId;
+module.exports.postUser = postUser;
+module.exports.putId = putId;
+module.exports.deleteId = deleteId;
+module.exports.getUsername = getUsername;
