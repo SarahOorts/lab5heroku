60388260066023632c394f28fa460d5622913038
SarahOorts <r0657122@student.thomasmore.be>
2021-03-24 12:20:40 +0100

    (fix) - install cors + deployed cors in app.js from lab5Heroku

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const routerMessages = require("./routes/api/v1/index");
+const cors = require("cors");
+
+app.use(cors()); // request nr server zoals cors anywhere mr ingesteld op u server ipv and server
 
 app.get("/", (req, res) => {
     app.set('view engine', "pug");
