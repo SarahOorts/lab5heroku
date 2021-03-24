372bd6ccbc10728ffb962a133619c73ef9b6e1c6
SarahOorts <r0657122@student.thomasmore.be>
2021-03-24 12:01:02 +0100

    (fix) - changed single to double quot line 6 of app.js Lab5Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routerMessages = require("./routes/api/v1/index");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     app.set('view engine', "pug");
 //   res.send('Hello World!')
   //homepage pug
