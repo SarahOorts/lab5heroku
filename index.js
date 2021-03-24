6f1e484a50f005d28ed304191cdb042c55195671
SarahOorts <r0657122@student.thomasmore.be>
2021-03-24 11:16:38 +0100

    (fix) - remove const port in index.js from lab5Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = process.env.PORT || 3000;
 const routerMessages = require("./routes/api/v1/index");
 
 app.get('/', (req, res) => {
